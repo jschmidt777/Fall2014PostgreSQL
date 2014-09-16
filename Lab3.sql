8993eb30d141ef301ae995389e36c09a5a908e72
jschmidt777 <schmidtjoseph39@gmail.com>
2014-09-16 16:04:11 -0400

    Lab3: almost complete
    Joseph Schmidt, 9/16/2014, Lab 3. I finished the questions, but I still
    have to test the queries more just to make sure.

--- Lab3.sql
+++ Lab3.sql
@@ -1,4 +1,8 @@
-﻿--Question1--
+﻿--Questions for Lab3--
+--Note: Some of the queries were tricky since some of the data is--
+--abreviated. For example, in question7, the months are abreviated.--
+
+--Question1--
 select name, city
 from Agents
 where name='Bond'
@@ -18,3 +22,18 @@ select name, city   --I put in city to show more of a distinction.--
 from Agents
 where city <> 'New York' and city <> 'London'
 --Question6--
+select *
+from products
+where city <> 'Dallas' and city <> 'Duluth' and priceUSD <= 1.00
+--Question7--
+select *
+from orders
+where mon = 'jan' or mon = 'apr'
+--Question8--
+select *
+from orders
+where mon = 'feb' and dollars > 200  --dollars is not a good title--
+--Question9--
+select *
+from orders
+where cid = 'c005' --c005 didn't order anything in this snapshot of the db--
