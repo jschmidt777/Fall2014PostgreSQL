1b6947dd700208718c02f1014ef01378a00a29c8
jschmidt777 <schmidtjoseph39@gmail.com>
2014-09-16 15:29:40 -0400

    Lab3: continued
    Joseph Schmidt, 9/16/2014, More of Lab 3. Looking pretty goo so far, if
    I do say so myself.

--- Lab3.sql
+++ Lab3.sql
@@ -7,3 +7,14 @@ select pid, name, quantity
 from products
 where priceUSD >= 0.99
 --Question3--
+select ordno, qty
+from orders
+--Question4--
+select name, city
+from customers
+where city = 'Duluth'
+--Question5--
+select name, city   --I put in city to show more of a distinction.--
+from Agents
+where city <> 'New York' and city <> 'London'
+--Question6--
