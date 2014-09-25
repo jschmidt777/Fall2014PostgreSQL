7858df4260787816a56bdf656e65bf79438cb122
jschmidt777 <schmidtjoseph39@gmail.com>
2014-09-25 13:24:21 -0400

    Lab4
    Joseph Schmidt, 9/25/2014, Lab4. Making this commit now just in case I
    wanted to revert for some reason. Got the first query, and pretty much
    understand how it works, but I wonder if it's okay that I used primary
    keys for the first nested subquery (I'm pretty sure it is, b/c otherwise
    these would be impossible).

---
+++ Lab4.sql
@@ -0,0 +1,68 @@
+﻿--Lab4 Assignment--
+--Joseph Schmidt, started 9/13/2014--
+
+--Question1--
+select distinct city
+from agents
+where city in(	select name 
+		from customers
+		where name='Tiptop')
+select city
+from agents
+where city in(	select cid 
+		from orders
+		where cid='c001')
+select distinct cid 
+from orders
+where cid='c001'
+
+
+
+select distinct city
+from Agents
+where city in ( select cid
+		from orders
+		where cid in (	select name
+				from customers
+				where name='Tiptop'))
+select name 
+from customers
+where name='Tiptop' 
+
+select name
+from customers
+where 'Tiptop' in (select distinct city
+			from Agents)
+--does this work--
+select distinct city 
+from Agents
+where 'ACME' in ( 	select name
+			from customers
+			where name = 'ACME')
+
+select name
+from customers
+where name in (		select distinct city
+			from agents
+			where name ='Tiptop')
+--Example subquery--
+select cid
+from orders
+where aid in (select aid
+              from agents
+              where city = 'Dallas')
+select distinct city
+from agents
+where aid in(
+	select distinct aid
+	from orders
+	where cid in (select cid
+		      from customers
+		      where name = 'Tiptop'))
+			
+--Question2--
+--Question3--
+select cid, name
+from customers
+where 
+
