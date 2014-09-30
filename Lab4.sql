d42e1ac7ada68ebef6bee6ab5aeb899947b2c21d
jschmidt777 <schmidtjoseph39@gmail.com>
2014-09-29 21:10:11 -0400

    Lab4: Almost done
    Joseph Schmidt, 9/29/2014, Lab4. I'm 95% sure I'm done, but I just have
    to test the queries more and fix up the formatting.

--- Lab4.sql
+++ Lab4.sql
@@ -14,7 +14,9 @@ where aid in(select distinct aid
 	     from orders
              where cid in (select cid
 			   from customers
-			   where name = 'Tiptop'))
+			   where name = 'Tiptop'
+			   )
+	    )
 			   			
 --Question2--
 --Get the pids of products ordered through and agent who makes at least one order for a customer in Kyoto.--
@@ -30,6 +32,32 @@ where pid in(select pid
 			  from customers
 			  where city = 'Kyoto'))
 			  --This needs more something but not nesting to include agents--
+select distinct pid
+from orders
+where aid in(select aid
+	     from orders
+	     where cid in (select cid 
+			   from customers
+			   where city = 'Kyoto'
+			   )
+	     )
+order by pid asc
+			--THIS IS THE ANSWER. IT SATISFYS THE CONDITION THAT THEY ORDERED FOR A CUSTOMER IN KYOTO AND THEN LISTS THE PRODUCTS THAT THEY ORDERED.--
+
+select distinct pid
+from products
+where pid in(select pid
+	     from orders
+	     where cid in((select cid 
+			   from customers
+			   where city = 'Kyoto'))
+	     intersect
+             select aid 
+	     from orders
+	     )
+
+			  --This is "right" but it's not right. I think I'm really close though.--
+			  --I think I some how need to include where exist in here.--
 
 --Question3--
 select cid, name
@@ -38,16 +66,22 @@ where cid not in (select cid
 		  from orders
 		  where aid in(select aid
 			       from agents	
-			       where aid = 'a04'))
+			       where aid = 'a04'
+			       )
+		 )
 --Question4--
 select cid, name
 from customers
-where cid in (select cid
+where cid in( select cid
 	      from orders
-	      where pid in (select pid
-			    from products
-			    where pid='p01'and pid='p07'))
-			    --This should be working but it doesn't and I can't figure out why.--
+              where pid='p01'
+	      intersect
+	      select cid
+	      from orders
+	      where pid='p07'
+			  
+	    )
+--Nailed it. Intersect was the syntax I was missing.--
 --Question5--
 select pid 
 from products
@@ -55,7 +89,9 @@ where pid in(select pid
 	     from orders
 	     where aid in(select aid 
 			  from agents
-			  where aid = 'a04'))
+			  where aid = 'a04'
+			  )
+	    )
 --Question6--
 select name, discount
 from customers 
@@ -63,12 +99,15 @@ where cid in (select cid
 	      from orders
 	      where aid in(select aid
 			   from agents
-			   where city in('Dallas', 'New York')))
+			   where city in('Dallas', 'New York')
+			   )
+	     )
 --Question7--
 select *
 from customers
 where discount in(select discount 
 		  from customers
-		  where city in('Dallas','Kyoto'))
+		  where city in('Dallas','Kyoto')
+		  )
 --This is right, but I just want to make sure--
 
