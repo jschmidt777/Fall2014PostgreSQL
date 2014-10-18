2febe42837140eb6ac134ed40304054af1a7f63b
jschmidt777 <schmidtjoseph39@gmail.com>
2014-10-17 21:43:43 -0400

    Lab6: Final Checks
    Joseph Schmidt, 10/17/14, Lab 6. Final checks done for this lab; this
    was definitely not easy.

--- Lab6.sql
+++ Lab6.sql
@@ -13,16 +13,18 @@ where count_cities.num_city = (select count(*)
 							   from products p
 							   group by p.city
 							   order by count(*) desc
-							   limit 1)
--- The answers are the same for questions one and two because even though there are two cities that make the most products--
---(Dallas and Newark) there are no customers from Newark ( Well, at least not yet; this should work for all time though).
+							   limit 1
+							  )
+-- The answers are the same for questions one and two because event though there are two cities that make the most products--
+--(Dallas and Newark) there are no customers from Newark ( Well, at least not yet; this will work for all time).
 
 --Question 3--
 --I was making this more complicated than it needed to be--
 select p.pid
 from products p
 where p.priceUSD > (select avg(priceUSD)
-					from products p)
+					from products p
+				   )
 --Question 4--
 select c.name, o.pid, o.dollars
 from orders o inner join customers c
@@ -30,14 +32,14 @@ on o.cid = c.cid
 order by o.dollars asc
 
 --Question 5--
-select count(o.ordno), c.name
+select c.name, count(o.ordno) 
 from customers c left outer join orders o
 on o.cid = c.cid
 where o.ordno is not null
    or o.ordno is null
 group by c.name
-order by c.name
--- So I think I've done it here. There is no need for coalesce because it already says there are no orders from Weyland.--
+order by c.name asc
+--This looks nice already, so coalesce was not needed.--
 
 --Question 6--
 select distinct c.name, a.name, p.name
@@ -51,29 +53,11 @@ where a.city = 'New York'
 order by c.name asc
 
 --Question 7--
-select o.dollars 
-from orders o full outer join (
-select p.priceUSD * o.qty as "Actual_Dollars"
-from products p inner join orders o
-on o.pid = p.pid         ) 
-as actual_dollars on actual_dollars = o.dollars
-
-with actual_dollars as (	
-			select p.priceUSD * o.qty as "Actual_Dollars"
-			from products p inner join orders o
-			on o.pid = p.pid  
-				)
-select o.dollars
-from orders o,
-     actual_dollars
-where actual_dollars = o.dollars
-
 select o.dollars
 from orders o
 where o.dollars not in ( select p.priceUSD * o.qty as "Actual_Dollars"
 						 from products p inner join orders o
 						 on o.pid = p.pid
 					   )
---This is totally right if we are allowed to use sub-queries, which I don't see why we wouldn't be allowed to anyways. Otherwise, I have the other join query above to work on.--		       
-
+--Finished 10/17/14--				
 
